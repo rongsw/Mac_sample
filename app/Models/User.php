b9987c642dbc728ba77c458ffc31b0cb5d0d5177
rongsw <525188983@qq.com>
2018-02-08 14:30:22 +0000

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -31,5 +31,15 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public static function boot(){
+
+        parent::boot();
+
+        static::creating(function ($user){
+
+            $user->activation_token = str_random(30);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,12 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+//Route::get('test', 'UsersController@create')->name('test');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
